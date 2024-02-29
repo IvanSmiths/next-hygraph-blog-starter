b2d6088747106f1f8ed96977b59f60ad7ab15f64
IvanSmiths <info@ivansmiths.com>
2024-02-29 14:25:00 +0100

    add query to fetch all blogposts

---
+++ utils/queries.tsx
@@ -0,0 +1,45 @@
+export type Posts = {
+    slug: string;
+    title: string;
+    excerpt: string;
+    date: string;
+};
+
+type QueryResult = {
+    posts: Posts[];
+};
+
+type Response = {
+    data: QueryResult;
+};
+async function getAllPosts(): Promise<Posts[]> {
+    if (!process.env.HYGRAPH_ENDPOINT) {
+        throw new Error("Environment variable HYGRAPH_ENDPOINT is not set.");
+    }
+    const response: globalThis.Response = await fetch(
+        process.env.HYGRAPH_ENDPOINT,
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+            },
+            body: JSON.stringify({
+                query: `
+                      query getAllPosts {
+                        posts(orderBy: publishedAt_DESC) {
+                          slug
+                          title
+                          excerpt
+                          date
+                        }
+                      }
+                    `,
+            }),
+        },
+    );
+    const { data }: Response = await response.json();
+    return data.posts;
+}
+
+export { getAllPosts }
