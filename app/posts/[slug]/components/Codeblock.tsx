63303b4d8489094c7f9e686e60194768876f2256
IvanSmiths <info@ivansmiths.com>
2024-03-01 23:31:29 +0100

    add copy to clipboard

--- app/posts/[slug]/components/Codeblock.tsx
+++ app/posts/[slug]/components/Codeblock.tsx
@@ -2,12 +2,50 @@
 
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { coldarkDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import { LinkEmbedProps } from "@graphcms/rich-text-types";
 
 const Codeblock: FC<LinkEmbedProps> = ({ children }) => {
+  const [isCopied, setIsCopied] = useState<Boolean>(false);
+  const copyCode = async () => {
+    try {
+      await navigator.clipboard.writeText(children);
+      setIsCopied(true);
+      setTimeout(() => setIsCopied(false), 1500);
+    } catch (err) {
+      console.error("Failed to copy code:", err);
+    }
+  };
+
+  useEffect(() => {
+    const removeListener = () =>
+      document.removeEventListener("click", handleClickOutside);
+    document.addEventListener("click", handleClickOutside);
+    return removeListener;
+  }, [isCopied]);
+
+  //@ts-ignore
+  const handleClickOutside = (event) => {
+    if (!event.target.closest(".codeblock")) {
+      setIsCopied(false);
+    }
+  };
+
   return (
-    <div className="bg-primaryLighter rounded-xl border-[1px] border-primaryLight flex items-center">
+    <div className="bg-primaryLighter rounded-xl border-[1px] border-primaryLight flex items-center relative">
+      <img
+        src="/icons/copy.svg"
+        height="42px"
+        width="42px"
+        alt="copy to clipboard"
+        onClick={copyCode}
+        className="absolute -right-5 -top-5 cursor-pointer"
+      />
+      {isCopied && (
+        <span className="p-3 rounded absolute m-auto left-0 right-0 bg-primary w-fit">
+          Copied!
+        </span>
+      )}
       <SyntaxHighlighter
         showLineNumbers={true}
         language="typescript"
