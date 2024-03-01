18e5a9b65e569b3ee90efe771180e14ed265f4f9
IvanSmiths <info@ivansmiths.com>
2024-03-01 12:14:32 +0100

    install graphcms/rich-text-react-renderer

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -3,6 +3,7 @@ import { notFound } from "next/navigation";
 import { PostProps } from "@/utils/types";
 import PillWrapper from "@/app/globalComponents/PillWrapper";
 import Time from "@/app/globalComponents/Time";
+import { RichText } from "@graphcms/rich-text-react-renderer";
 
 export default async function Page({ params }: PostProps) {
   const post: any = await getPost(params.slug);
@@ -17,8 +18,12 @@ export default async function Page({ params }: PostProps) {
     <article className="flex flex-col gap-small">
       <h1 className="text-3xl text-secondaryAccent">{post.title}</h1>
       <h2>{post.excerpt}</h2>
-      <Time date={post.date} />
+      <div className="flex items-center gap-2">
+        <dt className="text-sm font-medium leading-5">Published on:</dt>
+        <Time date={post.date} />
+      </div>
       <PillWrapper map={post} lightBg={false} />
+      <RichText content={post.content.raw} />
     </article>
   );
 }
