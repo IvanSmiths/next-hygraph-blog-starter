f451c5f634f58d18f423c4be655de4cfedb36d47
IvanSmiths <info@ivansmiths.com>
2024-03-03 20:44:55 +0100

    add alt text to image and improve responsiveness

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -24,17 +24,17 @@ export default async function Page({ params }: PostProps) {
     return notFound();
   }
 
-  console.log(post.category);
+  console.log(post);
 
   return (
-    <article className="flex flex-col gap-small mt-section">
+    <article className="flex flex-col gap-small sm:mt-section mt-medium">
       <div className="w-full flex justify-center">
-        <div className="flex flex-col gap-regular justify-center items-center w-10/12">
+        <div className="flex flex-col gap-regular justify-center items-center w-11/12">
           <Pill label={post.category} lightBg={false} />
-          <h1 className="md:text-6xl text-3xl text-center text-secondaryAccent md:px-10 lg:px-20 px-0">
+          <h1 className="md:text-6xl text-3xl text-center text-secondaryAccent md:px-10 lg:px-32 px-0">
             {post.title}
           </h1>
-          <h2 className="text-center md:px-10 lg:px-40 xl:px-56 px-0">
+          <h2 className="text-center md:px-10 lg:px-56 xl:px-64 px-0">
             {post.excerpt}
           </h2>
           <div className="w-full flex justify-center">
@@ -44,35 +44,30 @@ export default async function Page({ params }: PostProps) {
             <span className="text-sm font-medium leading-5">Published on:</span>
             <Time date={post.date} />
           </div>
-          <img className="w-full" src="/docs/blog-starter.png" alt="" />
+          <img
+            className="w-full"
+            src={post.coverImage.url}
+            height={post.coverImage.height}
+            width={post.coverImage.width}
+            loading="lazy"
+            alt=""
+          />
         </div>
       </div>
       <div className="pt-section flex flex-col items-center flex-wrap h-full w-full">
-        <div className="flex w-10/12">
-          <div className="lg:w-4/6 w-full flex flex-col gap-regular">
-            <RichText
-              content={post.content.raw}
-              renderers={{
-                h2: ({ children }) => (
-                  <h2 className={`text-3xl text-secondaryAccent`}>
-                    {children}
-                  </h2>
-                ),
-                p: ({ children }) => <h2 className={`text-md`}>{children}</h2>,
-                blockquote: ({ children }) => (
-                  <Blockquote>{children}</Blockquote>
-                ),
-                code_block: ({ children }) => <Codeblock>{children}</Codeblock>,
-              }}
-            />
-            {post.chart ? <Charts post={post} /> : null}
-          </div>
-          <ul className="w-2/6 sticky top-medium lg:flex flex-col items-end h-fit hidden lg:visible">
-            <li>Section 1</li>
-            <li>Section 2</li>
-            <li>Section 3</li>
-            <li>Section 4</li>
-          </ul>
+        <div className="lg:w-7/12 w-11/12 flex flex-col gap-regular">
+          <RichText
+            content={post.content.raw}
+            renderers={{
+              h2: ({ children }) => (
+                <h2 className={`text-3xl text-secondaryAccent`}>{children}</h2>
+              ),
+              p: ({ children }) => <h2 className={`text-md`}>{children}</h2>,
+              blockquote: ({ children }) => <Blockquote>{children}</Blockquote>,
+              code_block: ({ children }) => <Codeblock>{children}</Codeblock>,
+            }}
+          />
+          {post.chart ? <Charts post={post} /> : null}
         </div>
       </div>
     </article>
--- utils/queries.tsx
+++ utils/queries.tsx
@@ -70,6 +70,7 @@ async function getPost(slug: string): Promise<PostPage[]> {
                 url
                 width
                 height
+                altText
               }
               author {
                 ... on Author {
