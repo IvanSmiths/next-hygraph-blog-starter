047bb1c97e21f20df8d70bd06639a5ca07794034
IvanSmiths <info@ivansmiths.com>
2024-03-04 15:28:17 +0100

    put blob further up

--- app/globalComponents/Blob.tsx
+++ app/globalComponents/Blob.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 
 const Blob: FC = () => {
   return (
-    <div className="w-3/4 absolute -top-6 left-1/2 transform blur-[150px] md:blur-[200px] lg:blur-[300px] bg-brandAlpha -translate-x-1/2 h-96">
+    <div className="w-3/4 absolute -top-40 left-1/2 transform blur-[150px] md:blur-[200px] lg:blur-[300px] bg-brandAlpha -translate-x-1/2 h-96">
       <div className="bg-brandAlpha w-full h-full rounded-full"></div>
     </div>
   );
