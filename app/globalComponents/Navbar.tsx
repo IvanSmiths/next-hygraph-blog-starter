3f432dd4c06abc35d360fd7ecc76ca6992f807d2
IvanSmiths <info@ivansmiths.com>
2024-03-04 18:07:39 +0100

    set height to fit

--- app/globalComponents/Navbar.tsx
+++ app/globalComponents/Navbar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const Navbar: FC = () => {
   return (
     <header className="w-full h-fit flex flex-col items-center z-10">
-      <nav className="fixed sm:top-6 bottom-8 sm:right-auto right-8 sm:w-10/12 w-fit z-10">
+      <nav className="fixed sm:top-6 bottom-8 sm:right-auto right-8 h-fit sm:w-10/12 w-fit z-10">
         <Link className="inline-block w-fit" href="/">
           <img src="/logo.svg" alt="logo" height="40" width="40" />
         </Link>
