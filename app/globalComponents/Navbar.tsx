bac67de91ccf70672f2ff460d63dc466a1db4c33
IvanSmiths <info@ivansmiths.com>
2024-03-04 15:43:34 +0100

    put navbar on bottom right on mobile

--- app/globalComponents/Navbar.tsx
+++ app/globalComponents/Navbar.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 const Navbar: FC = () => {
   return (
     <header className="w-full flex flex-col items-center z-10">
-      <nav className="fixed top-6 w-10/12 z-10">
-        <Link href="/">
+      <nav className="fixed sm:top-6 bottom-8 sm:right-auto right-8 md:w-10/12 w-fit z-10">
+        <Link className="inline-block w-fit" href="/">
           <img src="/logo.svg" alt="logo" height="40" width="40" />
         </Link>
       </nav>
