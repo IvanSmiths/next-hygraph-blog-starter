9674dfe4f48a2cd87e14ad01dbe982ff63e00a54
IvanSmiths <info@ivansmiths.com>
2024-03-04 12:06:12 +0100

    add linear color to title

--- app/posts/[slug]/components/Hero.tsx
+++ app/posts/[slug]/components/Hero.tsx
@@ -10,7 +10,7 @@ const Hero: FC<Hero> = ({ post }) => {
     <div className="w-full flex justify-center">
       <div className="flex flex-col gap-regular justify-center items-center w-11/12">
         {post.category ? <Pill label={post.category} lightBg={false} /> : null}
-        <h1 className="md:text-6xl text-3xl text-center text-secondaryAccent md:w-9/12 :w-full">
+        <h1 className="md:text-7xl text-3xl text-center bg-gradient-to-r from-secondaryAccent via-secondaryAccent to-brand font-semibold text-transparent h-full pb-2 bg-clip-text md:w-9/12 :w-full">
           {post.title}
         </h1>
         <h2 className="text-center md:w-6/12 :w-full">{post.excerpt}</h2>
